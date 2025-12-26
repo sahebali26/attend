660461cd0a36646bc2f9775245fc8616ac182002
sahebali26 <sahebali8474@gmail.com>
2025-12-26 19:36:34 +0530

    create index.html

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'Student Profile-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://www.eduspark.site/assets/images/logo.png',
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
